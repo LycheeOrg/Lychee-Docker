acb299afedf9f2c8bc8633c0ff67f0d3441db3d9
Rob <rob.a.landry@gmail.com>
2019-01-27 13:22:08 -0500

    Adjust settings for first run

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,48 +1,34 @@
 #!/bin/bash
 
-echo "in entrypoint"
+echo "**** Starting the Entrypoint Script ****"
 set -e
 
-# create our folders
+
+echo "**** Make sure the /conf folder exists ****"
+[[ ! -f /conf ]] && \
 mkdir -p \
 	/conf
 
-echo "Copy config"
-# copy config
+
+echo "**** Copy the .env to /conf ****" && \
 [[ ! -e /conf/.env ]] && \
 	cp /var/www/html/Lychee-Laravel/.env.example /conf/.env
 [[ ! -L /var/www/html/Lychee-Laravel/.env ]] && \
 	ln -s /conf/.env /var/www/html/Lychee-Laravel/.env
 
-# Customize apache2.conf
-#[[ ! -e /etc/apache2/apache2.conf.bak ]] && \
-#	cp /etc/apache2/apache2.conf /etc/apache2/apache2.conf.bak && \
-#	echo "
-#<Directory /var/www/html/Lychee-Laravel>
-#	Options Indexes FollowSymLinks
-#	AllowOverride All
-#	Require all granted
-#</Directory>" >> /etc/apache2/apache2.conf
 
-echo "Permissions"
-# permissions
+echo "**** Set Permissions ****" && \
 chown -R abc:abc \
 	/conf \
 	/var/www/html/Lychee-Laravel
 
 
-
-echo "**** generate the key (to make sure that cookies cannot be decrypted etc) ****" && \
-cd /var/www/html/Lychee-Laravel && \
-./artisan key:generate
-
-
-
-echo "**** migrate the database ****" && \
-cd /var/www/html/Lychee-Laravel && \
-./artisan migrate
-
-
-echo "Start it"
-#systemctl restart apache2
-/etc/init.d/apache2 restart
+[[ ! -e /tmp/first_run ]] && \
+	echo "**** generate the key (to make sure that cookies cannot be decrypted etc) ****" && \
+	cd /var/www/html/Lychee-Laravel && \
+	./artisan key:generate && \
+	echo "**** migrate the database ****" && \
+	cd /var/www/html/Lychee-Laravel && \
+	./artisan migrate && \
+	touch /tmp/first_run && \
+	/etc/init.d/apache2 restart
