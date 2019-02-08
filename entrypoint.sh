58ba448ed34f767067604a024cbfcb9cf0599c3e
Rob <rob.a.landry@gmail.com>
2019-02-08 12:10:37 -0500

    Try inject.sh run

--- entrypoint.sh
+++ entrypoint.sh
@@ -21,9 +21,8 @@ echo "**** Copy the .env to /conf ****" && \
 	cp /var/www/html/Lychee-Laravel/.env.example /conf/.env
 [[ ! -L /var/www/html/Lychee-Laravel/.env ]] && \
 	ln -s /conf/.env /var/www/html/Lychee-Laravel/.env
-ls -la / && \
 echo "**** Inject .env values ****" && \
-	./inject.sh
+	/inject.sh
 
 
 [[ ! -e /tmp/first_run ]] && \
