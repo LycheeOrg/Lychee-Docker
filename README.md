c74f12c04725beb08efb0df0f09cb82646010557
Rob <rob.a.landry@gmail.com>
2019-02-08 13:31:34 -0500

    Update readme.

--- README.md
+++ README.md
@@ -1,16 +1,82 @@
-You must have a database docker running.
+## Prerequsites ##
 
-You will need to create the db, username, password.
+*  You must have a database docker running **OR** create one in your docker-compose.yml.
 
-Once the docker is run, you will need to edit the /conf/.env file with the appropriate info.
+## Setup ##
 
-```
-docker run -d
---name=lychee-laravel
--v /host_path/lychee-laravel/conf:/conf
--v /host_path/lychee-laravel/uploads:/uploads
--p 90:80
---net network_name
---link db_name
+1.  Create the db, username, password.
+2.  Edit the environment variables:
+    *  After the first run, edit the /conf/.env file with the appropriate info
+    *  **OR**
+    *  Supply the environment variables via docker run/docker-compose
+
+## Example docker run command ##
+
+1.  **Make sure that you link to the container running your database!!**  
+    The example below shows --net and --link for these purposes  
+    --net connects to the name of the network your database is on  
+    --link connects to the database  
+
+```bash
+docker run -d \
+--name=lychee-laravel \
+-v /host_path/lychee-laravel/conf:/conf \
+-v /host_path/lychee-laravel/uploads:/uploads \
+-e PUID=1000 \
+-e PGID=1000 \
+-e PHP_TZ=America/New_York \
+-e DB_CONNECTION=mysql \
+-e DB_HOST=mariadb \
+-e DB_PORT=3306 \
+-e DB_DATABASE=homestead \
+-e DB_USERNAME=homestead \
+-e DB_PASSWORD=secret \
+-p 90:80 \
+--net network_name \
+--link db_name \
 bigrob8181/lychee-laravel
 ```
+
+## Example docker-compose.yml ##
+
+[docker-compose.yml](https://gitlab.landry.me/Dockerfile/lychee-laravel/blob/master/docker-compose.yml)  
+<!--- [docker-compose-database.yml](https://gitlab.landry.me/Dockerfile/lychee-laravel/blob/master/docker-compose-database.yml) -->
+
+## Available Environment Variables and defaults ##
+
+PUID=1000  
+PGID=1000  
+PHP_TZ=America/New_York  
+APP_NAME=Laravel  
+APP_ENV=local  
+APP_KEY=base64:WQQ0sZoM0aIDMOn9P6axFOCZvrm4F6dTVb5mqCS0DU0=  
+APP_DEBUG=true  
+APP_URL=http://localhost  
+LOG_CHANNEL=stack  
+DB_CONNECTION=mysql  
+DB_HOST=mariadb  
+DB_PORT=3306  
+DB_DATABASE=homestead  
+DB_USERNAME=homestead  
+DB_PASSWORD=secret  
+DB_DROP_CLEAR_TABLES_ON_ROLLBACK=false  
+DB_OLD_LYCHEE_PREFIX=''  
+BROADCAST_DRIVER=log  
+CACHE_DRIVER=file  
+SESSION_DRIVER=file  
+SESSION_LIFETIME=120  
+QUEUE_DRIVER=sync  
+SECURITY_HEADER_HSTS_ENABLE=false  
+REDIS_HOST=127.0.0.1  
+REDIS_PASSWORD=null  
+REDIS_PORT=6379  
+MAIL_DRIVER=smtp  
+MAIL_HOST=smtp.mailtrap.io  
+MAIL_PORT=2525  
+MAIL_USERNAME=null  
+MAIL_PASSWORD=null  
+MAIL_ENCRYPTION=null  
+PUSHER_APP_ID=''  
+PUSHER_APP_KEY=''  
+PUSHER_APP_SECRET=''  
+PUSHER_APP_CLUSTER=mt1  
--- inject.sh
+++ inject.sh
@@ -1,37 +1,37 @@
 #!/bin/bash
 
-sed -i "s|APP_NAME=.*|APP_NAME=${APP_NAME}|i" /conf/.env && \
-sed -i "s|APP_ENV=.*|APP_ENV=${APP_ENV}|i" /conf/.env && \
-sed -i "s|APP_KEY=.*|APP_KEY=${APP_KEY}|i" /conf/.env && \
-sed -i "s|APP_DEBUG=.*|APP_DEBUG=${APP_DEBUG}|i" /conf/.env && \
-sed -i "s|APP_URL=.*|APP_URL=${APP_URL}|i" /conf/.env && \
-sed -i "s|LOG_CHANNEL=.*|LOG_CHANNEL=${LOG_CHANNEL}|i" /conf/.env && \
-sed -i "s|DB_CONNECTION=.*|DB_CONNECTION=${DB_CONNECTION}|i" /conf/.env && \
-sed -i "s|DB_HOST=.*|DB_HOST=${DB_HOST}|i" /conf/.env && \
-sed -i "s|DB_PORT=.*|DB_PORT=${DB_PORT}|i" /conf/.env && \
-sed -i "s|DB_DATABASE=.*|DB_DATABASE=${DB_DATABASE}|i" /conf/.env && \
-sed -i "s|DB_USERNAME=.*|DB_USERNAME=${DB_USERNAME}|i" /conf/.env && \
-sed -i "s|DB_PASSWORD=.*|DB_PASSWORD=${DB_PASSWORD}|i" /conf/.env && \
-sed -i "s|DB_DROP_CLEAR_TABLES_ON_ROLLBACK=.*|DB_DROP_CLEAR_TABLES_ON_ROLLBACK=${DB_DROP_CLEAR_TABLES_ON_ROLLBACK}|i" /conf/.env && \
-sed -i "s|DB_OLD_LYCHEE_PREFIX=.*|DB_OLD_LYCHEE_PREFIX=${DB_OLD_LYCHEE_PREFIX}|i" /conf/.env && \
-sed -i "s|BROADCAST_DRIVER=.*|BROADCAST_DRIVER=${BROADCAST_DRIVER}|i" /conf/.env && \
-sed -i "s|CACHE_DRIVER=.*|CACHE_DRIVER=${CACHE_DRIVER}|i" /conf/.env && \
-sed -i "s|SESSION_DRIVER=.*|SESSION_DRIVER=${SESSION_DRIVER}|i" /conf/.env && \
-sed -i "s|SESSION_LIFETIME=.*|SESSION_LIFETIME=${SESSION_LIFETIME}|i" /conf/.env && \
-sed -i "s|QUEUE_DRIVER=.*|QUEUE_DRIVER=${QUEUE_DRIVER}|i" /conf/.env && \
-sed -i "s|SECURITY_HEADER_HSTS_ENABLE=.*|SECURITY_HEADER_HSTS_ENABLE=${SECURITY_HEADER_HSTS_ENABLE}|i" /conf/.env && \
-sed -i "s|REDIS_HOST=.*|REDIS_HOST=${REDIS_HOST}|i" /conf/.env && \
-sed -i "s|REDIS_PASSWORD=.*|REDIS_PASSWORD=${REDIS_PASSWORD}|i" /conf/.env && \
-sed -i "s|REDIS_PORT=.*|REDIS_PORT=${REDIS_PORT}|i" /conf/.env && \
-sed -i "s|MAIL_DRIVER=.*|MAIL_DRIVER=${MAIL_DRIVER}|i" /conf/.env && \
-sed -i "s|MAIL_HOST=.*|MAIL_HOST=${MAIL_HOST}|i" /conf/.env && \
-sed -i "s|MAIL_PORT=.*|MAIL_PORT=${MAIL_PORT}|i" /conf/.env && \
-sed -i "s|MAIL_USERNAME=.*|MAIL_USERNAME=${MAIL_USERNAME}|i" /conf/.env && \
-sed -i "s|MAIL_PASSWORD=.*|MAIL_PASSWORD=${MAIL_PASSWORD}|i" /conf/.env && \
-sed -i "s|MAIL_ENCRYPTION=.*|MAIL_ENCRYPTION=${MAIL_ENCRYPTION}|i" /conf/.env && \
-sed -i "s|PUSHER_APP_ID=.*|PUSHER_APP_ID=${PUSHER_APP_ID}|i" /conf/.env && \
-sed -i "s|PUSHER_APP_KEY=.*|PUSHER_APP_KEY=${PUSHER_APP_KEY}|i" /conf/.env && \
-sed -i "s|PUSHER_APP_SECRET=.*|PUSHER_APP_SECRET=${PUSHER_APP_SECRET}|i" /conf/.env && \
-sed -i "s|PUSHER_APP_CLUSTER=.*|PUSHER_APP_CLUSTER=${PUSHER_APP_CLUSTER}|i" /conf/.env && \
-sed -i "s|;*date.timezone =.*|date.timezone = ${PHP_TZ}|i" /etc/php/7.3/apache2/php.ini && \
+sed -i "s|APP_NAME=.*|APP_NAME=${APP_NAME}|i" /conf/.env
+sed -i "s|APP_ENV=.*|APP_ENV=${APP_ENV}|i" /conf/.env
+sed -i "s|APP_KEY=.*|APP_KEY=${APP_KEY}|i" /conf/.env
+sed -i "s|APP_DEBUG=.*|APP_DEBUG=${APP_DEBUG}|i" /conf/.env
+sed -i "s|APP_URL=.*|APP_URL=${APP_URL}|i" /conf/.env
+sed -i "s|LOG_CHANNEL=.*|LOG_CHANNEL=${LOG_CHANNEL}|i" /conf/.env
+sed -i "s|DB_CONNECTION=.*|DB_CONNECTION=${DB_CONNECTION}|i" /conf/.env
+sed -i "s|DB_HOST=.*|DB_HOST=${DB_HOST}|i" /conf/.env
+sed -i "s|DB_PORT=.*|DB_PORT=${DB_PORT}|i" /conf/.env
+sed -i "s|DB_DATABASE=.*|DB_DATABASE=${DB_DATABASE}|i" /conf/.env
+sed -i "s|DB_USERNAME=.*|DB_USERNAME=${DB_USERNAME}|i" /conf/.env
+sed -i "s|DB_PASSWORD=.*|DB_PASSWORD=${DB_PASSWORD}|i" /conf/.env
+sed -i "s|DB_DROP_CLEAR_TABLES_ON_ROLLBACK=.*|DB_DROP_CLEAR_TABLES_ON_ROLLBACK=${DB_DROP_CLEAR_TABLES_ON_ROLLBACK}|i" /conf/.env
+sed -i "s|DB_OLD_LYCHEE_PREFIX=.*|DB_OLD_LYCHEE_PREFIX=${DB_OLD_LYCHEE_PREFIX}|i" /conf/.env
+sed -i "s|BROADCAST_DRIVER=.*|BROADCAST_DRIVER=${BROADCAST_DRIVER}|i" /conf/.env
+sed -i "s|CACHE_DRIVER=.*|CACHE_DRIVER=${CACHE_DRIVER}|i" /conf/.env
+sed -i "s|SESSION_DRIVER=.*|SESSION_DRIVER=${SESSION_DRIVER}|i" /conf/.env
+sed -i "s|SESSION_LIFETIME=.*|SESSION_LIFETIME=${SESSION_LIFETIME}|i" /conf/.env
+sed -i "s|QUEUE_DRIVER=.*|QUEUE_DRIVER=${QUEUE_DRIVER}|i" /conf/.env
+sed -i "s|SECURITY_HEADER_HSTS_ENABLE=.*|SECURITY_HEADER_HSTS_ENABLE=${SECURITY_HEADER_HSTS_ENABLE}|i" /conf/.env
+sed -i "s|REDIS_HOST=.*|REDIS_HOST=${REDIS_HOST}|i" /conf/.env
+sed -i "s|REDIS_PASSWORD=.*|REDIS_PASSWORD=${REDIS_PASSWORD}|i" /conf/.env
+sed -i "s|REDIS_PORT=.*|REDIS_PORT=${REDIS_PORT}|i" /conf/.env
+sed -i "s|MAIL_DRIVER=.*|MAIL_DRIVER=${MAIL_DRIVER}|i" /conf/.env
+sed -i "s|MAIL_HOST=.*|MAIL_HOST=${MAIL_HOST}|i" /conf/.env
+sed -i "s|MAIL_PORT=.*|MAIL_PORT=${MAIL_PORT}|i" /conf/.env
+sed -i "s|MAIL_USERNAME=.*|MAIL_USERNAME=${MAIL_USERNAME}|i" /conf/.env
+sed -i "s|MAIL_PASSWORD=.*|MAIL_PASSWORD=${MAIL_PASSWORD}|i" /conf/.env
+sed -i "s|MAIL_ENCRYPTION=.*|MAIL_ENCRYPTION=${MAIL_ENCRYPTION}|i" /conf/.env
+sed -i "s|PUSHER_APP_ID=.*|PUSHER_APP_ID=${PUSHER_APP_ID}|i" /conf/.env
+sed -i "s|PUSHER_APP_KEY=.*|PUSHER_APP_KEY=${PUSHER_APP_KEY}|i" /conf/.env
+sed -i "s|PUSHER_APP_SECRET=.*|PUSHER_APP_SECRET=${PUSHER_APP_SECRET}|i" /conf/.env
+sed -i "s|PUSHER_APP_CLUSTER=.*|PUSHER_APP_CLUSTER=${PUSHER_APP_CLUSTER}|i" /conf/.env
+sed -i "s|;*date.timezone =.*|date.timezone = ${PHP_TZ}|i" /etc/php/7.3/apache2/php.ini
 sed -i "s|;*date.timezone =.*|date.timezone = ${PHP_TZ}|i" /etc/php/7.3/cli/php.ini
