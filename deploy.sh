b3d31b2f2ec46c0f160e76feb3f033eecb1efc12
roblandry <rob.a.landry@gmail.com>
2019-02-17 13:22:40 -0500

    Testing cleanup. Update Readme.md (#9)

--- deploy.sh
+++ deploy.sh
@@ -2,11 +2,11 @@
 
 docker login -u $REGISTRY_USER -p $REGISTRY_PASS
 
-echo "TRAVIS_BRANCH: $TRAVIS_BRANCH"
-echo "TRAVIS_TAG: $TRAVIS_TAG"
-echo "TRAVIS_PULL_REQUEST: $TRAVIS_PULL_REQUEST"
-echo "TRAVIS_EVENT_TYPE: $TRAVIS_EVENT_TYPE"
-echo "TRAVIS_PULL_REQUEST_BRANCH: $TRAVIS_PULL_REQUEST_BRANCH"
+#echo "TRAVIS_BRANCH: $TRAVIS_BRANCH"
+#echo "TRAVIS_TAG: $TRAVIS_TAG"
+#echo "TRAVIS_PULL_REQUEST: $TRAVIS_PULL_REQUEST"
+#echo "TRAVIS_EVENT_TYPE: $TRAVIS_EVENT_TYPE"
+#echo "TRAVIS_PULL_REQUEST_BRANCH: $TRAVIS_PULL_REQUEST_BRANCH"
 
 # if its a tagged version
 if [[ -n "$TRAVIS_TAG" ]]; then
@@ -27,33 +27,4 @@ else
   echo "Pushing testing"
   docker tag $REPO':'$TRAVIS_BUILD_NUMBER $REPO':testing'
   docker push $REPO':testing'
-
 fi
-
-
-# notes:
-# create pr branch = testing
-
-# create pr = testing
-#    CI-BRANCH
-#    TRAVIS_BRANCH: CI-PR2
-#    TRAVIS_TAG:
-#    TRAVIS_PULL_REQUEST: false
-#    TRAVIS_EVENT_TYPE: push
-#    TRAVIS_PULL_REQUEST_BRANCH:
-#
-#    CI-PR
-#    TRAVIS_BRANCH: master
-#    TRAVIS_TAG:
-#    TRAVIS_PULL_REQUEST: 7
-#    TRAVIS_EVENT_TYPE: pull_request
-#    TRAVIS_PULL_REQUEST_BRANCH: CI-PR2
-
-# merge pr = ? should be dev
-#    TRAVIS_BRANCH: master
-#    TRAVIS_TAG:
-#    TRAVIS_PULL_REQUEST: false
-#    TRAVIS_EVENT_TYPE: push
-#    TRAVIS_PULL_REQUEST_BRANCH:
-
-# create tag = tag/latest
