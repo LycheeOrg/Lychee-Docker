3c8b747cb67e00ce6a68f005b50e765d4d01ed9a
Martin Stone <martin@d7415.co.uk>
2020-02-23 17:51:18 +0000

    Switch to Lycheebot Docker Hub account. Add (unused) deploy script for PRs.

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-docker login -u $REGISTRY_USER -p $REGISTRY_PASS
+echo "$REGISTRY_PASS" | docker login -u $REGISTRY_USER --password-stdin
 
 #echo "TRAVIS_BRANCH: $TRAVIS_BRANCH"
 #echo "TRAVIS_TAG: $TRAVIS_TAG"
@@ -10,7 +10,7 @@ docker login -u $REGISTRY_USER -p $REGISTRY_PASS
 
 # if its a tagged version
 if [[ -n "$TRAVIS_TAG" ]]; then
-  echo "Building mulit arch and pushing tagged version and latest"
+  echo "Building multi arch and pushing tagged version and latest"
   docker buildx build \
     --progress plain \
     --platform linux/arm/v7,linux/arm/v6,linux/arm64,linux/amd64 \
@@ -21,11 +21,21 @@ if [[ -n "$TRAVIS_TAG" ]]; then
 
 # if its a merged pr or nightly
 elif [[ "$TRAVIS_BRANCH" == "master" && "$TRAVIS_PULL_REQUEST" == "false" ]]; then
-  echo "Building mulit arch and pushing dev"
+  echo "Building multi arch and pushing dev"
   docker buildx build \
     --progress plain \
     --platform linux/arm/v7,linux/arm/v6,linux/arm64,linux/amd64 \
     -t $DOCKER_REPO':dev' \
+    --push \
+    .
+
+# if its a pr
+elif [[ "$TRAVIS_PULL_REQUEST" != "false" ]]; then
+  echo "Building multi arch and pushing pr tag"
+  docker buildx build \
+    --progress plain \
+    --platform linux/arm/v7,linux/arm/v6,linux/arm64,linux/amd64 \
+    -t $DOCKER_REPO':pr$TRAVIS_PULL_REQUEST' \
     --push \
     .
 
