fcc57a6d5e1bbce80b80b804716768878f83f660
Michael MACHADO <michael.machado@passculture.app>
2021-03-28 19:22:16 +0200

    feat(env_var) Add PHP_POST_MAX_SIZE env var

--- inject.sh
+++ inject.sh
@@ -98,3 +98,6 @@ fi
 if [ "$PHP_TZ" != '' ]; then
     sed -i "s|;*date.timezone =.*|date.timezone = ${PHP_TZ}|i" /etc/php/7.4/cli/php.ini
 fi
+if [ "$PHP_POST_MAX_SIZE" != '' ]; then
+    sed -i "s|;*post_max_size =.*|post_max_size = ${PHP_POST_MAX_SIZE}|i" /etc/php/7.4/cli/php.ini
+fi
