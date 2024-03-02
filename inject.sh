f04ad8a403b0da3a6ef09738d86b89f6a958053d
Benoît Viguier <ildyria@users.noreply.github.com>
2024-03-02 12:31:24 +0100

    Better injection + add options in docker compose (#186)
    * better injection + add options in docker compose
    
    * Use pipes instead of slashes with sed to handle timezones like Europe/Berlin
    
    ---------
    
    Co-authored-by: Martin Stone <martin@d7415.co.uk>

--- inject.sh
+++ inject.sh
@@ -1,135 +1,140 @@
 #!/bin/bash
+function replace_or_insert() {
+    # Voodoo magic: https://superuser.com/a/976712
+    grep -q "^${1}=" /conf/.env && sed "s|^${1}=.*|${1}=${2}|" -i /conf/.env || sed "$ a\\${1}=${2}" -i /conf/.env
+}
+
 if [ "$APP_NAME" != '' ]; then
-    sed -i "s|APP_NAME=.*|APP_NAME=${APP_NAME}|i" /conf/.env
-fi
+    replace_or_insert "APP_NAME" "$APP_NAME"
+  fi
 if [ "$APP_ENV" != '' ]; then
-    sed -i "s|APP_ENV=.*|APP_ENV=${APP_ENV}|i" /conf/.env
-fi
+    replace_or_insert "APP_ENV" "$APP_ENV"
+ fi
 if [ "$APP_DEBUG" != '' ]; then
-    sed -i "s|APP_DEBUG=.*|APP_DEBUG=${APP_DEBUG}|i" /conf/.env
-fi
+    replace_or_insert "APP_DEBUG" "$APP_DEBUG"
+ fi
 if [ "$APP_URL" != '' ]; then
-    sed -i "s|APP_URL=.*|APP_URL=${APP_URL}|i" /conf/.env
-fi
+    replace_or_insert "APP_URL" "$APP_URL"
+ fi
 if [ "$APP_FORCE_HTTPS" != '' ]; then
-    sed -i "s|APP_FORCE_HTTPS=.*|APP_FORCE_HTTPS=${APP_FORCE_HTTPS}|i" /conf/.env
-fi
+    replace_or_insert "APP_FORCE_HTTPS" "$APP_FORCE_HTTPS"
+ fi
 if [ "$APP_DIR" != '' ]; then
-    sed -i "s|APP_DIR=.*|APP_DIR=${APP_DIR}|i" /conf/.env
-fi
+    replace_or_insert "APP_DIR" "$APP_DIR"
+ fi
 if [ "$DEBUGBAR_ENABLED" != '' ]; then
-    sed -i "s|DEBUGBAR_ENABLED=.*|DEBUGBAR_ENABLED=${DEBUGBAR_ENABLED}|i" /conf/.env
-fi
+    replace_or_insert "DEBUGBAR_ENABLED" "$DEBUGBAR_ENABLED"
+ fi
 if [ "$LEGACY_V4_REDIRECT" != '' ]; then
-    sed -i "s|LEGACY_V4_REDIRECT=.*|LEGACY_V4_REDIRECT=${LEGACY_V4_REDIRECT}|i" /conf/.env
-fi
+    replace_or_insert "LEGACY_V4_REDIRECT" "$LEGACY_V4_REDIRECT"
+ fi
 if [ "$DB_OLD_LYCHEE_PREFIX" != '' ]; then
-    sed -i "s|DB_OLD_LYCHEE_PREFIX=.*|DB_OLD_LYCHEE_PREFIX=${DB_OLD_LYCHEE_PREFIX}|i" /conf/.env
-fi
+    replace_or_insert "DB_OLD_LYCHEE_PREFIX" "$DB_OLD_LYCHEE_PREFIX"
+ fi
 if [ "$DB_CONNECTION" != '' ]; then
-    sed -i "s|DB_CONNECTION=.*|DB_CONNECTION=${DB_CONNECTION}|i" /conf/.env
-fi
+    replace_or_insert "DB_CONNECTION" "$DB_CONNECTION"
+ fi
 if [ "$DB_HOST" != '' ]; then
-    sed -i "s|DB_HOST=.*|DB_HOST=${DB_HOST}|i" /conf/.env
-fi
+    replace_or_insert "DB_HOST" "$DB_HOST"
+ fi
 if [ "$DB_PORT" != '' ]; then
-    sed -i "s|DB_PORT=.*|DB_PORT=${DB_PORT}|i" /conf/.env
-fi
+    replace_or_insert "DB_PORT" "$DB_PORT"
+ fi
 if [ "$DB_DATABASE" != '' ]; then
-    sed -i "s|DB_DATABASE=.*|DB_DATABASE=${DB_DATABASE}|i" /conf/.env
-fi
+    replace_or_insert "DB_DATABASE" "$DB_DATABASE"
+ fi
 if [ "$DB_USERNAME" != '' ]; then
-    sed -i "s|DB_USERNAME=.*|DB_USERNAME=${DB_USERNAME}|i" /conf/.env
-fi
+    replace_or_insert "DB_USERNAME" "$DB_USERNAME"
+ fi
 if [ "$DB_PASSWORD" != '' ]; then
-    sed -i "s|DB_PASSWORD=.*|DB_PASSWORD=${DB_PASSWORD}|i" /conf/.env
-elif [ "$DB_PASSWORD_FILE" != '' ]; then
+    replace_or_insert "DB_PASSWORD" "$DB_PASSWORD"
+ elif [ "$DB_PASSWORD_FILE" != '' ]; then
     value=$(<$DB_PASSWORD_FILE)
-    sed -i "s|DB_PASSWORD=.*|DB_PASSWORD=${value}|i" /conf/.env
-fi
+    replace_or_insert "DB_PASSWORD" "$value"
+ fi
 if [ "$DB_LOG_SQL" != '' ]; then
-    sed -i "s|DB_LOG_SQL=.*|DB_LOG_SQL=${DB_LOG_SQL}|i" /conf/.env
-fi
+    replace_or_insert "DB_LOG_SQL" "$DB_LOG_SQL"
+ fi
 if [ "$DB_LOG_SQL_EXPLAIN" != '' ]; then
-    sed -i "s|DB_LOG_SQL_EXPLAIN=.*|DB_LOG_SQL_EXPLAIN=${DB_LOG_SQL_EXPLAIN}|i" /conf/.env
-fi
+    replace_or_insert "DB_LOG_SQL_EXPLAIN" "$DB_LOG_SQL_EXPLAIN"
+ fi
 if [ "$TIMEZONE" != '' ]; then
-    sed -i "s|TIMEZONE=.*|TIMEZONE=${TIMEZONE}|i" /conf/.env
-fi
+    replace_or_insert "TIMEZONE" "$TIMEZONE"
+ fi
 if [ "$ENABLE_TOKEN_AUTH" != '' ]; then
-    sed -i "s|ENABLE_TOKEN_AUTH=.*|ENABLE_TOKEN_AUTH=${ENABLE_TOKEN_AUTH}|i" /conf/.env
-fi
+    replace_or_insert "ENABLE_TOKEN_AUTH" "$ENABLE_TOKEN_AUTH"
+ fi
 if [ "$CACHE_DRIVER" != '' ]; then
-    sed -i "s|CACHE_DRIVER=.*|CACHE_DRIVER=${CACHE_DRIVER}|i" /conf/.env
-fi
+    replace_or_insert "CACHE_DRIVER" "$CACHE_DRIVER"
+ fi
 if [ "$SESSION_DRIVER" != '' ]; then
-    sed -i "s|SESSION_DRIVER=.*|SESSION_DRIVER=${SESSION_DRIVER}|i" /conf/.env
-fi
+    replace_or_insert "SESSION_DRIVER" "$SESSION_DRIVER"
+ fi
 if [ "$SESSION_LIFETIME" != '' ]; then
-    sed -i "s|SESSION_LIFETIME=.*|SESSION_LIFETIME=${SESSION_LIFETIME}|i" /conf/.env
-fi
+    replace_or_insert "SESSION_LIFETIME" "$SESSION_LIFETIME"
+ fi
 if [ "$QUEUE_CONNECTION" != '' ]; then
-    sed -i "s|QUEUE_DRIVER=.*|QUEUE_DRIVER=${QUEUE_DRIVER}|i" /conf/.env
-fi
+    replace_or_insert "QUEUE_CONNECTION" "$QUEUE_CONNECTION"
+ fi
 if [ "$SECURITY_HEADER_HSTS_ENABLE" != '' ]; then
-    sed -i "s|SECURITY_HEADER_HSTS_ENABLE=.*|SECURITY_HEADER_HSTS_ENABLE=${SECURITY_HEADER_HSTS_ENABLE}|i" /conf/.env
-fi
+    replace_or_insert "SECURITY_HEADER_HSTS_ENABLE" "$SECURITY_HEADER_HSTS_ENABLE"
+ fi
 if [ "$SECURITY_HEADER_CSP_CONNECT_SRC" != '' ]; then
-    sed -i "s|SECURITY_HEADER_CSP_CONNECT_SRC=.*|SECURITY_HEADER_CSP_CONNECT_SRC=${SECURITY_HEADER_CSP_CONNECT_SRC}|i" /conf/.env
-fi
+    replace_or_insert "SECURITY_HEADER_CSP_CONNECT_SRC" "$SECURITY_HEADER_CSP_CONNECT_SRC"
+ fi
 if [ "$SECURITY_HEADER_SCRIPT_SRC_ALLOW" != '' ]; then
-    sed -i "s|SECURITY_HEADER_SCRIPT_SRC_ALLOW=.*|SECURITY_HEADER_SCRIPT_SRC_ALLOW=${SECURITY_HEADER_SCRIPT_SRC_ALLOW}|i" /conf/.env
-fi
+    replace_or_insert "SECURITY_HEADER_SCRIPT_SRC_ALLOW" "$SECURITY_HEADER_SCRIPT_SRC_ALLOW"
+ fi
 if [ "$SESSION_SECURE_COOKIE" != '' ]; then
-    sed -i "s|SESSION_SECURE_COOKIE=.*|SESSION_SECURE_COOKIE=${SESSION_SECURE_COOKIE}|i" /conf/.env
-fi
-if [ "$REDIS_SCHEME" != '' ]; then
-    sed -i "s|REDIS_SCHEME=.*|REDIS_SCHEME=${REDIS_SCHEME}|i" /conf/.env
-fi
-if [ "$REDIS_PATH" != '' ]; then
-    sed -i "s|REDIS_PATH=.*|REDIS_PATH=${REDIS_PATH}|i" /conf/.env
-fi
-if [ "$REDIS_HOST" != '' ]; then
-    sed -i "s|REDIS_HOST=.*|REDIS_HOST=${REDIS_HOST}|i" /conf/.env
-fi
-if [ "$REDIS_PORT" != '' ]; then
-    sed -i "s|REDIS_PORT=.*|REDIS_PORT=${REDIS_PORT}|i" /conf/.env
-fi
-if [ "$REDIS_PASSWORD" != '' ]; then
-    sed -i "s|REDIS_PASSWORD=.*|REDIS_PASSWORD=${REDIS_PASSWORD}|i" /conf/.env
-elif [ "$REDIS_PASSWORD_FILE" != '' ]; then
-    value=$(<$REDIS_PASSWORD_FILE)
-    sed -i "s|REDIS_PASSWORD=.*|REDIS_PASSWORD=${value}|i" /conf/.env
-fi
+    replace_or_insert "SESSION_SECURE_COOKIE" "$SESSION_SECURE_COOKIE"
+ fi
+# if [ "$REDIS_SCHEME" != '' ]; then
+#     sed -i "s|REDIS_SCHEME=.*|REDIS_SCHEME=${REDIS_SCHEME}|i" /conf/.env
+# fi
+# if [ "$REDIS_PATH" != '' ]; then
+#     sed -i "s|REDIS_PATH=.*|REDIS_PATH=${REDIS_PATH}|i" /conf/.env
+# fi
+# if [ "$REDIS_HOST" != '' ]; then
+#     sed -i "s|REDIS_HOST=.*|REDIS_HOST=${REDIS_HOST}|i" /conf/.env
+# fi
+# if [ "$REDIS_PORT" != '' ]; then
+#     sed -i "s|REDIS_PORT=.*|REDIS_PORT=${REDIS_PORT}|i" /conf/.env
+# fi
+# if [ "$REDIS_PASSWORD" != '' ]; then
+#     sed -i "s|REDIS_PASSWORD=.*|REDIS_PASSWORD=${REDIS_PASSWORD}|i" /conf/.env
+# elif [ "$REDIS_PASSWORD_FILE" != '' ]; then
+#     value=$(<$REDIS_PASSWORD_FILE)
+#     sed -i "s|REDIS_PASSWORD=.*|REDIS_PASSWORD=${value}|i" /conf/.env
+# fi
 if [ "$MAIL_DRIVER" != '' ]; then
-    sed -i "s|MAIL_DRIVER=.*|MAIL_DRIVER=${MAIL_DRIVER}|i" /conf/.env
+    replace_or_insert "MAIL_DRIVER" "$MAIL_DRIVER"
 fi
 if [ "$MAIL_HOST" != '' ]; then
-    sed -i "s|MAIL_HOST=.*|MAIL_HOST=${MAIL_HOST}|i" /conf/.env
+    replace_or_insert "MAIL_HOST" "$MAIL_HOST"
 fi
 if [ "$MAIL_PORT" != '' ]; then
-    sed -i "s|MAIL_PORT=.*|MAIL_PORT=${MAIL_PORT}|i" /conf/.env
+    replace_or_insert "MAIL_PORT" "$MAIL_PORT"
 fi
 if [ "$MAIL_USERNAME" != '' ]; then
-    sed -i "s|MAIL_USERNAME=.*|MAIL_USERNAME=${MAIL_USERNAME}|i" /conf/.env
+    replace_or_insert "MAIL_USERNAME" "$MAIL_USERNAME"
 fi
 if [ "$MAIL_PASSWORD" != '' ]; then
-    sed -i "s|MAIL_PASSWORD=.*|MAIL_PASSWORD=${MAIL_PASSWORD}|i" /conf/.env
+    replace_or_insert "MAIL_PASSWORD" "$MAIL_PASSWORD"
 elif [ "$MAIL_PASSWORD_FILE" != '' ]; then
     value=$(<$MAIL_PASSWORD_FILE)
-    sed -i "s|MAIL_PASSWORD=.*|MAIL_PASSWORD=${value}|i" /conf/.env
+    replace_or_insert "MAIL_PASSWORD" "$value"
 fi
 if [ "$MAIL_ENCRYPTION" != '' ]; then
-    sed -i "s|MAIL_ENCRYPTION=.*|MAIL_ENCRYPTION=${MAIL_ENCRYPTION}|i" /conf/.env
+    replace_or_insert "MAIL_ENCRYPTION" "$MAIL_ENCRYPTION"
 fi
 if [ "$MAIL_FROM_NAME" != '' ]; then
-    sed -i "s|MAIL_FROM_NAME=.*|MAIL_FROM_NAME=${MAIL_FROM_NAME}|i" /conf/.env
+    replace_or_insert "MAIL_FROM_NAME" "$MAIL_FROM_NAME"
 fi
 if [ "$MAIL_FROM_ADDRESS" != '' ]; then
-    sed -i "s|MAIL_FROM_ADDRESS=.*|MAIL_FROM_ADDRESS=${MAIL_FROM_ADDRESS}|i" /conf/.env
+    replace_or_insert "MAIL_FROM_ADDRESS" "$MAIL_FROM_ADDRESS"
 fi
 if [ "$TRUSTED_PROXIES" != '' ]; then
-    sed -i "s|TRUSTED_PROXIES=.*|TRUSTED_PROXIES=${TRUSTED_PROXIES}|i" /conf/.env
+    replace_or_insert "TRUSTED_PROXIES" "$TRUSTED_PROXIES"
 fi
 if [ "$PHP_TZ" != '' ]; then
     sed -i "s|;*date.timezone =.*|date.timezone = ${PHP_TZ}|i" /etc/php/8.2/cli/php.ini
